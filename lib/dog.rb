7ba405f2a00f6c2dc0ca79c22b3b39055a6ad406
Marc Poppy <poppy.marc@gmail.com>
2019-05-30 04:52:16 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 
 class Dog
-  def name=(dog_name)
+  def name=(dog_name) #this is the writer method
     @my_name = dog_name
   end
-  def name
+  def name #this is the reader method
     @my_name
   end
 end
