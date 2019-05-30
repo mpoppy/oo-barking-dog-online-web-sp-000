75fcd194f524526103ce978df79eb9b86ff76b4d
Marc Poppy <poppy.marc@gmail.com>
2019-05-30 04:53:55 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+
+class Dog
+  def name=(dog_name) #this is the writer method
+    @my_name = dog_name
+  end
+  def name #this is the reader method
+    @my_name
+  end
+  def bark
+    puts "woof!"
+  end
+end
+
+fido = Dog.new
+fido.name = "Fido"
+fido.bark
+
+puts fido.name
